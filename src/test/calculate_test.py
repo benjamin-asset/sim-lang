30ede2111be2647bb778cedbafcdea63ff94e2af
Dongkyoo <wind.dong.dream@gmail.com>
2021-03-07 13:04:18 +0900

    동시에 여러 테스트가 가능하도록 calculate unit, calculate 클래스화

--- src/test/calculate_test.py
+++ src/test/calculate_test.py
@@ -1,9 +1,10 @@
 from language.language_utils import import_module
-import language.execute.calculator as calculator
+from language.execute.calculator import Calculator
 
 import datetime
 import time
 from utils.parameter import Market
+
 
 Indicator = import_module('utils', 'indicator')
 Function = import_module('utils', 'function')
@@ -16,6 +17,7 @@ def test_basic():
     to_date = datetime.date(2017, 12, 31)
 
     now = time.time()
+    calculator = Calculator()
     result = calculator.calculate(
         """
         (ts_delay(increase_from_lowest_price(low, close, 3), 1) >= 0.1 or ts_delay(increase_from_lowest_price(low, close, 3), 2) >= 0.1) and decrease_from_highest_price(high, close, 3) < 0 and decrease_from_highest_price(high, close, 3) > -0.1 and ibs(high, low, close) < 0.25 and rank(sma(tr_val, 5)) > 0.8
@@ -34,17 +36,17 @@ def test_basic():
         to_date
     )
 
-    print('result time : {:.3f}s'.format(time.time() - now))
-    result = to_test_format(result, from_date, to_date)
-
-    now = time.time()
     test_result = run(from_date, to_date)
-    test_result = to_test_format(test_result, from_date, to_date)
-    print('test result time : {:.3f}s'.format(time.time() - now))
+    assert check(result, test_result, from_date, to_date)
 
-    result = result.to_dict()
-    test_result = test_result.to_dict()
-    assert result == test_result
+
+def check(lhs, rhs, from_date, to_date):
+    lhs = to_test_format(lhs, from_date, to_date)
+    rhs = to_test_format(rhs, from_date, to_date)
+
+    lhs = lhs.to_dict()
+    rhs = rhs.to_dict()
+    return lhs == rhs
 
 
 def to_test_format(df, from_date: datetime.date, to_date: datetime.date):
@@ -52,4 +54,8 @@ def to_test_format(df, from_date: datetime.date, to_date: datetime.date):
     sorted_result = result.sort_values(by=['date', '#priority', 'open', 'close'])
     subset = sorted_result[['date', 'ticker_id', '#priority']]
     subset = subset[(subset['date'] >= from_date) & (subset['date'] <= to_date)]
-    return subset
+    return subset.reset_index(drop=True)
+
+
+if __name__ == '__main__':
+    test_basic()
